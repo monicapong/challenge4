3e9ab67f9bc71e4fa8e0892dd9f1297039d36c5c
monicapong <111400935+monicapong@users.noreply.github.com>
2022-10-25 22:44:03 -0400

    added notes to js code

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,25 @@
+//Access element using querySelector
+var viewHighScore = document.querySelector('.view_highscores');
+var timeEl = document.querySelector('.time');
 var intro = document.querySelector('.intro');
-var quiz = document.querySelector('.quiz');
-var checkGuess = document.querySelector('.check_guess');
 var startBtn = document.querySelector('.start_button');
+var quiz = document.querySelector('.quiz');
+var askQuestion = document.querySelector('.ask_question');
+var guess = document.querySelectorAll(".multiple_choice");
+var answer1 = document.querySelector('#answer1');
+var answer2 = document.querySelector('#answer2');
+var answer3 = document.querySelector('#answer3');
+var answer4 = document.querySelector('#answer4');
+var checkGuess = document.querySelector('.check_guess');
 var completedQuiz = document.querySelector('.completed_quiz');
 var finalScore = document.querySelector('.final_score')
+var finish = document.querySelector('.finish');
+var highScores = document.querySelector('.high_scores');
+var scoreList = document.querySelector('.score_list');
+var initialsInput = document.querySelector('#initials');
+var submitBtn = document.querySelector('.submit_button');
+var backBtn = document.querySelector('.back_btn');
+var clearBtn = document.querySelector('.clear_btn');
 
 //Question bank for the coding quiz challenge
 var questionBank = [
@@ -34,28 +50,11 @@ var questionBank = [
     }
 ];
 
-var askQuestion = document.querySelector('.ask_question');
-
-var guess = document.querySelectorAll(".multiple_choice");
-var answer1 = document.querySelector('#answer1');
-var answer2 = document.querySelector('#answer2');
-var answer3 = document.querySelector('#answer3');
-var answer4 = document.querySelector('#answer4');
-
+var scores = [];
 var questionNumber = 0;
 var questionCount = 1;
+//Set timer to 75 seconds
 var secondsLeft = 75;
-
-var timeEl = document.querySelector('.time');
-var finish = document.querySelector('.finish');
-var viewHighScore = document.querySelector('.view_highscores');
-var highScores = document.querySelector('.high_scores');
-var scoreList = document.querySelector('.score_list');
-var scores = [];
-var initialsInput = document.querySelector('#initials');
-var submitBtn = document.querySelector('.submit_button');
-var backBtn = document.querySelector('.back_btn');
-var clearBtn = document.querySelector('.clear_btn');
 
 function setTime() {
     //Sets interval in variable
@@ -147,14 +146,14 @@ function renderScores() {
     scoreList.innerHTML = "";
     scoreList.style.display ='block';
 
-    var highScore = sort();
+    var highScores = sort();
 
     //Render new li for each score
     for (var i = 0; i < scores.length; i++) {
-        var item = highScore[i];
+        var highScore = highScores[i];
 
         var li = document.createElement("li");
-        li.textContent = item.initials + " - " +item.score;
+        li.textContent = highScore.initials + " - " +highScore.score;
         li.setAttribute("data-index", i);
         li.style.backgroundColor = 'rgb(' + [210, 186, 236, 0.531].join(',') + ')'; 
         scoreList.appendChild(li);
@@ -167,10 +166,11 @@ function init() {
     //Retrieve stored scores from local storage
     var storedScores = JSON.parse(localStorage.getItem("scores"));
     
-    //Create an array for the scores
+    //If stored value exists, set scores to those values
     if (storedScores !== null) {
         scores = storedScores;
     } else {
+        //If stored doesn't exist, set scores to an empty array.
         scores = [];
     };
     return scores;
